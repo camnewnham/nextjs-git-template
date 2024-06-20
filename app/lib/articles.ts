01697d2a46489aa3eed145ebff2baaa7f0382901
cam <camnewnham@gmail.com>
2024-06-20 10:49:09 +1000

    use ARTICLES_PATH to specify markdown file root directory

--- app/lib/articles.ts
+++ app/lib/articles.ts
@@ -1,27 +1,38 @@
 import fs from "fs";
 import child_process from "child_process";
 
-const DOCS_DIR = "./docs";
+const ARTICLES_PATH = process.env.ARTICLES_PATH;
 
-const files = fs.readdirSync(DOCS_DIR);
+if (!ARTICLES_PATH) {
+  throw new Error("ARTICLES_PATH is not defined");
+}
 
-export const articles = files.map((file) => {
-  // Use git to get the last modified date of the file
-  const path = `${DOCS_DIR}/${file}`;
-
-  const { content, frontmatter } = parseFrontmatter(path);
-
-  const fileNameWithoutExtension = file.replace(/.md$/, "");
-
-  return {
-    fileName: file,
-    filePath: path,
-    title: frontmatter.title,
-    slug: fileNameWithoutExtension,
-    content: content,
-    ...getModifications(path),
-  };
+const files = fs.readdirSync(ARTICLES_PATH, {
+  recursive: true,
+  encoding: "utf-8",
 });
+
+export const articles = files
+  .filter((file) => file.includes(".md"))
+  .map((file) => {
+    // Use git to get the last modified date of the file
+    const path = `${ARTICLES_PATH}\\${file}`;
+
+    const { content, frontmatter } = parseFrontmatter(path);
+
+    const slug = file.replace(/.md$/, "").replace(/[\s\\]/g, "-");
+
+    // Extract the first h1 from the markdown
+    const title = content.match(/^#\s(.*)$/m)?.[1] || frontmatter.title;
+
+    return {
+      file,
+      title,
+      slug,
+      content,
+      ...getModifications(path),
+    };
+  });
 
 function getModifications(file: string) {
   const stdout = child_process
---
+++ test_articles/docs/other/test page 3.md
@@ -0,0 +1,21 @@
+---
+title: Test Post 2
+tags: test
+---
+
+# My Markdown File
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Code Blocks](#code-blocks)
+- [Footnotes](#footnotes)
+- [Task Lists](#task-lists)
+
+## Introduction
+
+Welcome to my Markdown file! This file demonstrates some less commonly used features of Markdown.
+
+## Code Blocks
+
+You can include code blocks in Markdown by using triple backticks (\`\`\`) followed by the language name. For example, here's a code block in Python:
