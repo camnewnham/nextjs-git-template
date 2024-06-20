c9a8779c4e6140aeda3fc54e0ec98497d3d31f07
cam <camnewnham@gmail.com>
2024-06-20 14:56:06 +1000

    add process.env.UNDER_CONSTRUCTION

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { PaginatedArticles } from "./list/[pagination]/ArticlesList";
 
 export default function Home() {
+  if (process.env.UNDER_CONSTRUCTION) {
+    return <div>Under Construction...</div>;
+  }
+
   return <PaginatedArticles pageNumber={1} />;
 }
